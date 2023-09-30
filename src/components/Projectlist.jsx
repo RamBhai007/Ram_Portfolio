44a69eb638cabca1568f52b7dce5df2ecad5da20
RammohanRockz <101466286+RamBhai007@users.noreply.github.com>
2023-09-30 07:35:26 +0000

    disney

--- src/components/Projectlist.jsx
+++ src/components/Projectlist.jsx
@@ -1,6 +1,7 @@
 import ProjectCard from "../components/Projectcard";
 import { Container, Row, Col } from "react-bootstrap";
 import netfliximage from '../assets/images/netflix.jpg'
+import DisneyImage from '../assets/images/Disney.png'
 import "../pages/style.css";
 
 
@@ -17,6 +18,15 @@ function Projectlist() {
                 description="This project is a simplified front-end clone of Netflix. It was created with React js. It uses TMDB (The Movie Data Base ) API and uses the Axios tool. Users can click movie images with embedded YouTube trailers or related videos about the movie."
                 ghLink="https://rambhai007.github.io/Netflix_clone/"
               />
+              
+            </Col>
+            <Col md={4} className="project-card">
+              <ProjectCard
+                imgPath={DisneyImage}
+                title="Disney-CLone"
+                description="This project is a simplified front-end clone of Disney. It was created with vite+React js. It uses TMDB (The Movie Data Base ) API and uses the Axios tool. "
+                ghLink="https://rambhai007.github.io/DisneyClone/"
+                />
             </Col>
           </Row>
         </Container>
