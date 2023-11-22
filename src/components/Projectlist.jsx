038531d982061eddd7a08854891e6c9efa5477b1
RammohanRockz <101466286+RamBhai007@users.noreply.github.com>
2023-11-22 12:09:25 +0530

    Update Projectlist.jsx

--- src/components/Projectlist.jsx
+++ src/components/Projectlist.jsx
@@ -2,6 +2,7 @@ import ProjectCard from "../components/Projectcard";
 import { Container, Row, Col } from "react-bootstrap";
 import netfliximage from '../assets/images/netflix.jpg'
 import DisneyImage from '../assets/images/Disney.png'
+import msm from '../assets/images/msm.png'
 import "../pages/style.css";
 
 
@@ -26,6 +27,14 @@ function Projectlist() {
                 title="Disney-CLone"
                 description="This project is a simplified front-end clone of Disney. It was created with vite+React js. It uses TMDB (The Movie Data Base ) API and uses the Axios tool. "
                 ghLink="https://rambhai007.github.io/DisneyClone/"
+                />
+            </Col>
+            <Col md={4} className="project-card">
+              <ProjectCard
+                imgPath={msm}
+                title="MSM-Unify"
+                description="MSM Unify is a student marketplace that connects students with educational institutions and recruitment partners worldwide. We unify and allow everyone in the industry to work collaboratively to reach the world's curious learners and provide them with various options regarding their education."
+                ghLink="https://www.msmunify.com/"
                 />
             </Col>
           </Row>
